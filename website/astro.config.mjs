4a82194ed1d0e8b6eca5380fecd9cf20df1dab94
flowcore-platform <gitops@flowcore.io>
2023-11-01 10:42:39 +0000

    refactor: added support for tailwind with a base theme

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,18 +1,24 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://docs.flowcore.io/',
-	integrations: [
-		starlight({
-			title: 'Flowcore Platform Documentation',
-			social: {
-				linkedin: 'https://www.linkedin.com/company/flowcore',
-			},
-			editLink: {
-				baseUrl: 'https://github.com/flowcore-io/astro-flowcore-docs/tree/main/website',
-			}
-		}),
-	],
+  site: 'https://docs.flowcore.io/',
+  integrations: [starlight({
+    title: 'Flowcore Platform Documentation',
+    social: {
+      linkedin: 'https://www.linkedin.com/company/flowcore'
+    },
+    editLink: {
+      baseUrl: 'https://github.com/flowcore-io/astro-flowcore-docs/tree/main/website'
+    },
+    customCss: [
+      // Path to your Tailwind base styles:
+      './src/tailwind.css',
+    ],
+  }), tailwind({
+    // Disable the default base styles:
+    applyBaseStyles: false
+  })]
 });
